d989bbb853784d4a6ac56f218f5a1b9725209437
soo <114020164+soojin-dev@users.noreply.github.com>
2022-12-17 02:00:32 +0900

    Add files via upload

---
+++ src/main/java/com/example/redisprac/Person.java
@@ -0,0 +1,28 @@
+package com.example.redisprac;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@RedisHash(value = "people", timeToLive = 30)
+public class Person {
+
+    @Id
+    private String id;
+    private String name;
+    private Integer age;
+    private LocalDateTime createdAt;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/redisprac/RedisPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPracApplication.class, args);
+    }
+
+}
